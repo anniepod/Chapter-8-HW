4d675dc90ee39eb00d46137bee2b6f67dff62b0a
anniepod <31967599+anniepod@users.noreply.github.com>
2018-02-05 19:36:59 -0500

    new

--- Ch8Question9.py
+++ Ch8Question9.py
@@ -1,11 +1,12 @@
 # Write a function that removes all occurrences of a given letter from a string:
 
 def remove_letter(letter, word):
-    new = ""
-    for char in word:
-        if char == letter:
-            new = word[1:char] + word[char:]
-            print(new)
+    new = 0
+    while new < len(word):
+        if word[new] == letter:
+            final = word[:letter] + word[letter:]
+        new += 1
+    return final
 
 
 remove_letter("a", "apple")
